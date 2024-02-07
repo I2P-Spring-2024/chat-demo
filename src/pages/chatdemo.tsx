7b8d6f64eecf828ec4faeafbfd0bf9aca94c9296
ChristineYiyunHu <yunyun10180328@yahoo.com>
2024-02-07 15:51:35 -0500

    Remove unused line

--- src/pages/chatdemo.tsx
+++ src/pages/chatdemo.tsx
@@ -1,6 +1,5 @@
 import React, { useState, useEffect, useRef } from 'react';
 import axios from 'axios';
-import { convertCompilerOptionsFromJson } from 'typescript';
 
 interface Message {
   text: string;
